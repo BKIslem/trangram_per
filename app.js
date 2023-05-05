ebfd11cb18051f3d386be8f3b2ce9e30d863e0bc
Geoffrey.Noel <126774531+Geoffrey184@users.noreply.github.com>
2023-05-05 14:27:04 +0200

    add avion

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const btnVautour = document.getElementById('btnFive');
 const btnCat = document.getElementById('btnTwo');
 const btnButterfly = document.getElementById('btnThree');
 const btnBird = document.getElementById('btnSix');
+const btnAvion = document.getElementById('btnSeven')
 const figures = document.querySelectorAll('.figure');
 
 
@@ -13,42 +14,50 @@ btnVautour.innerHTML = 'Vautour';
 btnCat.innerHTML= 'cat';
 btnButterfly.innerHTML= 'butterfly';
 btnBird.innerHTML = 'bird';
+btnAvion.innerHTML = 'Avion';
 
 btnCarre.addEventListener('click', () => {
     figures.forEach(elements => {
-        elements.classList.remove('giraffe','vautour', 'cat','butterfly', 'bird')
+        elements.classList.remove('giraffe','vautour', 'cat','butterfly', 'bird','avion')
     })
 })
 
 btnGiraffe.addEventListener('click', () => {
     figures.forEach(elements => {
         elements.classList.add('giraffe')
-        elements.classList.remove('vautour', 'cat','butterfly', 'bird')
+        elements.classList.remove('vautour', 'cat','butterfly', 'bird','avion')
     })
 })
 btnVautour.addEventListener('click', () => {
     figures.forEach(elements => {
         elements.classList.add('vautour')
-        elements.classList.remove('giraffe', 'cat','butterfly', 'bird')
+        elements.classList.remove('giraffe', 'cat','butterfly', 'bird','avion')
     
     })
 })
 btnCat.addEventListener('click', () => {
     figures.forEach(elements => {
         elements.classList.add('cat')
-        elements.classList.remove('vautour','butterfly', 'bird', 'giraffe')
+        elements.classList.remove('vautour','butterfly', 'bird', 'giraffe','avion')
     })
 })
 btnButterfly.addEventListener('click', () => {
     figures.forEach(elements => {
         elements.classList.add('butterfly')
-        elements.classList.remove('vautour','giraffe', 'cat', 'bird')
+        elements.classList.remove('vautour','giraffe', 'cat', 'bird','avion')
     })
 })
 
 btnBird.addEventListener('click', () => {
     figures.forEach(elements => {
         elements.classList.add('bird')
-        elements.classList.remove('vautour','giraffe', 'cat', 'butterfly')
+        elements.classList.remove('vautour','giraffe', 'cat', 'butterfly','avion')
+    })
+})
+
+btnAvion.addEventListener('click', () => {
+    figures.forEach(elements => {
+        elements.classList.add('avion')
+        elements.classList.remove('vautour','giraffe', 'cat', 'butterfly','bird')
     })
 })
