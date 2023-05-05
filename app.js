1aa61f2765f9308a2a47d30abd230778e92dd7db
BKIslem <benkhaled.islem3@gmail.com>
2023-05-05 11:13:44 +0200

    commitislem

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const btnGiraffe = document.getElementById('btnFour');
 const btnCarre = document.getElementById('btnOne');
 const btnVautour = document.getElementById('btnFive');
+const btnCat = document.getElementById('btnTwo');
+const btnButterfly = document.getElementById('btnThree');
 const figures = document.querySelectorAll('.figure');
 
 
 btnCarre.innerHTML = 'Carré';
 btnGiraffe.innerHTML = 'Giraffe';
 btnVautour.innerHTML = 'Vautour';
+btnCat.innerHTML= 'cat';
+btnButterfly.innerHTML= 'butterfly';
 btnCarre.addEventListener('click', () => {
     figures.forEach(elements => {
         elements.classList.remove('giraffe')
         elements.classList.remove('vautour')
+        elements.classList.remove('cat')
+        elements.classList.remove('butterfly')
     })
 })
 
@@ -18,11 +24,32 @@ btnGiraffe.addEventListener('click', () => {
     figures.forEach(elements => {
         elements.classList.add('giraffe')
         elements.classList.remove('vautour')
+        elements.classList.remove('cat')
+        elements.classList.remove('butterfly')
     })
 })
 btnVautour.addEventListener('click', () => {
     figures.forEach(elements => {
         elements.classList.add('vautour')
         elements.classList.remove('giraffe')
+        elements.classList.remove('cat')
+        elements.classList.remove('butterfly')
     })
 })
+btnCat.addEventListener('click', () => {
+    figures.forEach(elements => {
+        elements.classList.add('cat')
+        elements.classList.remove('vautour')
+        elements.classList.remove('giraffe')
+        elements.classList.remove('butterfly')
+    })
+})
+btnButterfly.addEventListener('click', () => {
+    figures.forEach(elements => {
+        elements.classList.add('butterfly')
+        elements.classList.remove('vautour')
+        elements.classList.remove('giraffe')
+        elements.classList.remove('cat')
+    })
+})
+
